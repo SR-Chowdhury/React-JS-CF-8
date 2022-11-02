88f365bcba00f0e15029b0085a74a3f808eba8b7
Shihan Chowdhury <hackedgdown@gmail.com>
2022-11-02 20:30:55 +0000

    Almost done

---
+++ src/uncontrolled-form/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class UncontrolledForm extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center my-5">Bismillah, Core React JS: Chapter Eight - Uncontrolled Form</h1>
+            </div>
+        );
+    }
+}
+
+export default UncontrolledForm;
